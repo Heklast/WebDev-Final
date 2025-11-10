3dc69eaa5f872d0bd448d977086364b7e3049a75
MahmoudElSayed120 <152595004+MahmoudElSayed120@users.noreply.github.com>
2025-11-11 00:26:59 +0100

    Sales provider with createSale and loadBookSales

---
+++ react-app/src/books/providers/useSalesProvider.tsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import type { CreateSaleModel, SaleModel } from '../SaleModel';
+
+export const useSalesProvider = () => {
+    const createSale = (input: CreateSaleModel) => {
+        return axios.post('http://localhost:3000/sales', input)
+    }
+
+    const loadBookSales = (bookId: string) => {
+        return axios.get<SaleModel[] | {data: SaleModel[]}>(
+            `http://localhost:3000/sales?bookId/sales`,
+        )
+    }
+    return { createSale, loadBookSales }
+}
