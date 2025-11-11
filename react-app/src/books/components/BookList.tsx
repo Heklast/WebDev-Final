a7bacb344fe9c29dc6f36d070e7f6f43da86d43f
MahmoudElSayed120 <152595004+MahmoudElSayed120@users.noreply.github.com>
2025-11-11 15:38:46 +0100

    Added search filter to book list

--- react-app/src/books/components/BookList.tsx
+++ react-app/src/books/components/BookList.tsx
@@ -1,21 +1,31 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useBookProvider } from '../providers/useBookProvider'
 import { BookListItem } from './BookListItem'
 import { CreateBookModal } from './CreateBookModal'
-
+import { Input } from 'antd'
 export function BookList() {
   const { books, loadBooks, deleteBook, updateBook, createBook } =
     useBookProvider()
+
 
   useEffect(() => {
     loadBooks()
   }, [])
 
+  const [query, setQuery] = useState<string>('')
+  const filteredBooks = books.filter(b => b.title.toLowerCase().includes(query.toLowerCase()),
+  ) 
+  
   return (
     <>
+      <Input.Search
+        placeholder = "Search books"
+        onChange = {e => setQuery(e.target.value)}
+        style = {{ margin: '1rem 0', width: '50%'}}
+        />
       <CreateBookModal onCreate={createBook} />
       <div style={{ padding: '0 .5rem' }}>
-        {books.map(book => (
+        {filteredBooks.map(book => (
           <BookListItem
             key={book.id}
             book={book}
