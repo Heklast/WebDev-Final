d93d88d4509ca440e43779e359c5e0a24234e802
MahmoudElSayed120 <152595004+MahmoudElSayed120@users.noreply.github.com>
2025-11-13 07:51:02 +0100

    Root and Layout fixed

--- react-app/src/routes/__root.tsx
+++ react-app/src/routes/__root.tsx
@@ -1,23 +1,14 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { Layout } from '../Layout'
 
-// Clients routes
-import { clientsRoute } from './clients'
-import { clientDetailsRoute } from './clients.$clientId'
+const RootLayout = () => {
+  return (
+    <Layout>
+      <Outlet />
+    </Layout>
+  )
+}
 
-// Import your original index route (default page)
-import { Route as IndexRoute } from './index'
-
-const RootLayout = () => (
-  <Layout>
-    <Outlet />
-  </Layout>
-)
-
-export const rootRoute = createRootRoute({ component: RootLayout })
-
-export const routeTree = rootRoute._addFileChildren({
-  IndexRoute, // ✅ Default page
-  clientsRoute,
-  clientDetailsRoute,
+export const Route = createRootRoute({
+  component: RootLayout,
 })
